a6833f84773c6912a8aaa4c564f7676752ac2f44
Yeison Stiven Builes Uribe <yeisonplox@gmail.com>
2021-03-20 09:09:42 -0500

    Moidal logic created succesfully

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const galleryImgs = document.querySelectorAll(".gallery .card img");
+const galleryImgs = document.querySelectorAll(".gallery .card img"); //All the images 
 const imgModal = document.querySelector(".modal img")
 const closeModal = document.querySelector(".modal .bx-x");
 const nextImage = document.querySelector(".modal .bx-right-arrow-alt")
@@ -7,28 +7,36 @@ const modal = document.querySelector(".modal")
 var imgsLength = galleryImgs.length
 var imgIndexCount = 0
 
-/*nextImage.onclick = switchImage(1);
-backImage.onclick = switchImage(-1);*/
-for(i=0;i<galleryImgs.length;i++){
-    console.log(galleryImgs[i].src)
-}
-nextImage.addEventListener('click',switchImage(1));
-backImage.addEventListener('click',switchImage(-1));
+nextImage.addEventListener('click',(evt)=>switchImage(1));
+backImage.addEventListener('click',(evt)=> switchImage(-1));
 closeModal.addEventListener('click', closeModalFunc);
 galleryImgs.forEach((images) => {
     images.addEventListener('click', (event) => {
-        modal.style.display = "inherit";
+        modal.style.display = "inherit"; //display modal
         let srcImage = event.target.src
         imgModal.src = srcImage
+
+        for(i=0;i<imgsLength;i++){
+            //get actual img index
+            if(srcImage == galleryImgs[i].src){
+                imgIndexCount = i
+            }
+        }
     });
 });
 
 function switchImage(n){
+    //increase or decrease index and get/set image. If index is less or more than the count of all of the images, change index
     imgIndexCount +=n
-    console.log(imgIndexCount)
+    if(imgIndexCount>=imgsLength){
+        imgIndexCount = 0
+    }else if(imgIndexCount < 0){
+        imgIndexCount = imgsLength -1
+    }
+    let srcImage = galleryImgs[imgIndexCount].src
+    imgModal.src=srcImage
 }
 
 function closeModalFunc() {
     modal.style.display = "none";
-    console.log(imgIndexCount)
 }
